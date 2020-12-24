d4ab35565fa3acaf86893a2352b4fd385c7ff35e
xingyu wang <brandon.xyw@gmail.com>
2020-12-24 12:22:18 -0800

    IBO, VBO

--- Mimic/Mimic/src/Application.cpp
+++ Mimic/Mimic/src/Application.cpp
@@ -1,6 +1,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 int main(void)
 {
@@ -28,11 +31,48 @@ int main(void)
 
 
 
+    float positions[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
+    // VBO
+    VertexBuffer VBO(positions, sizeof(positions));
+
+    // IBO
+    IndexBuffer IBO(indices, 6);
+
+    // VAO
+    unsigned int VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader shader("res/Shaders/basic.shader");
+    shader.Bind();
+
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        IBO.Bind();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
